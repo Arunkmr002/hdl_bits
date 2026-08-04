b4d4972dfdc9b8f82febbed72e4f73386a6e50f5
Arunkmr002 <arunkumar456429@gmail.com>
2026-08-04 09:26:14 +0530

    Create gernerate_for_loop_100_digit_bcd_adder.v

---
+++ more_verilog_features/gernerate_for_loop_100_digit_bcd_adder.v
@@ -0,0 +1,18 @@
+module top_module( 
+    input [399:0] a, b,
+    input cin,
+    output cout,
+    output [399:0] sum );
+    
+    wire [100:0]c;
+    assign c[0] = cin;
+    
+    genvar i;
+    
+    generate
+        for(i=0;i<100;i=i+1)begin : bcd
+            bcd_fadd dut (.a(a[4*i+3:4*i]),.b(b[4*i+3:4*i]),.cin(c[i]),.sum(sum[4*i+3:4*i]),.cout(c[i+1]));
+        end
+    endgenerate
+    assign cout = c[100];
+endmodule
